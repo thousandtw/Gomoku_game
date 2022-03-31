3456037a6cb169c816b56296b9c33bdb4cfc6dff
thousandtw <h0916916145@gmail.com>
2022-03-31 19:19:40 +0800

    Demo1

--- Gomoku/Gomoku/Form1.Designer.cs
+++ Gomoku/Gomoku/Form1.Designer.cs
@@ -29,13 +29,38 @@ namespace Gomoku
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Gomoku.Properties.Resources.black;
+            this.pictureBox1.Location = new System.Drawing.Point(406, 181);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::Gomoku.Properties.Resources.board;
+            this.ClientSize = new System.Drawing.Size(796, 699);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "五子棋";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
