a91c56200262f22b4f617738a1ca8543aef4f004
thousandtw <h0916916145@gmail.com>
2022-04-02 15:33:22 +0800

    Form1 重構

--- Gomoku/Gomoku/Form1.cs
+++ Gomoku/Gomoku/Form1.cs
@@ -12,12 +12,7 @@ namespace Gomoku
 {
     public partial class Form1 : Form
     {
-        //宣告自行建立的棋盤變數
-        private Board board = new Board();
-
-        //用於強制黑白交替
-        private PieceType nextPieceType = PieceType.Black;
-
+        private GameMaster game = new GameMaster();
 
         public Form1()
         {
@@ -27,29 +22,18 @@ namespace Gomoku
         // MouseEventArgs 紀錄滑鼠按下時的資訊
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            Piece piece = board.PlaceAPiece(e.X, e.Y, nextPieceType);
-
+            Piece piece = game.PlaceAPiece(e.X, e.Y);
             //判斷點上有無棋子
             if (piece != null)
             {
                 this.Controls.Add(piece);
-
-                //如果為白棋,下個棋則黑(反之)
-                if (nextPieceType == PieceType.Black)
-                {
-                    nextPieceType = PieceType.White;
-                }
-                else if (nextPieceType == PieceType.White)
-                {
-                    nextPieceType = PieceType.Black;
-                }
             }
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
             //判斷游標是否在能感應區域,即為能放棋子處
-            if (board.CanBePlaced(e.X, e.Y))
+            if (game.CanBePlaced(e.X, e.Y))
             {
                 //如果為能放棋子處,即改變游標圖案
                 this.Cursor = Cursors.Hand;
---
+++ Gomoku/Gomoku/GameMaster.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomoku
+{
+    //遊戲邏輯程式碼 (規矩)
+    class GameMaster
+    {
+        //宣告自行建立的棋盤變數
+        private Board board = new Board();
+
+        //用於強制黑白交替
+        private PieceType currentPlayer = PieceType.Black;
+
+        public bool CanBePlaced(int x, int y)
+        {
+            return board.CanBePlaced(x, y);
+        }
+
+        public Piece PlaceAPiece(int x, int y)
+        {
+            Piece piece = board.PlaceAPiece(x, y, currentPlayer);
+
+            //判斷點上有無棋子
+            if (piece != null)
+            {
+                //如果為白棋,下個棋則黑(反之)
+                if (currentPlayer == PieceType.Black)
+                {
+                    currentPlayer = PieceType.White;
+                }
+                else if (currentPlayer == PieceType.White)
+                {
+                    currentPlayer = PieceType.Black;
+                }
+
+                return piece;
+            }
+
+            return null;
+        }
+    }
+}
